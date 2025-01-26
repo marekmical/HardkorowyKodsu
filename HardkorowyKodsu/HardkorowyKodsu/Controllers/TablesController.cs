a5471038a7e306c548f80bea7edc6ca1bd3ed54b
marekmical <56329747+marekmical@users.noreply.github.com>
2025-01-26 13:39:02 +0100

    Add Server .NET Core Web API (#1)
    Added .gitignore
    Created ASP.NET Core Web API solution
    Used Microsoft.Data.SqlClient package for communication with local SQLExpress instance
    Db connection settings are in Configuration/DbConnection.cs
    Two available controllers - TablesController and ColumnsController with single REST GET methods
    API available at https://localhost:7152
    Swagger available at https://localhost:7152/swagger/index.html

---
+++ HardkorowyKodsu/HardkorowyKodsu/Controllers/TablesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace HardkorowyKodsu.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TablesController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public TablesController()
+        {
+            _connectionString = Configuration.DbConnection.ConnectionString;
+        }
+
+        // GET: api/Tables
+        [HttpGet]
+        public IEnumerable<string> GetTablesAndViews()
+        {
+            using (SqlConnection _connection = new SqlConnection(_connectionString))
+            {
+                _connection.Open();
+
+                List<string> tables = new List<string>();
+                DataTable dataTable = _connection.GetSchema("Tables");
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    string tablename = (string)row[2];
+                    tables.Add(tablename);
+                }
+
+                _connection.Close();
+
+                return tables;
+            }
+        }
+    }
+}
