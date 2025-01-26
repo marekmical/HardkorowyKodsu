55afb660b8471eb8e123e8b868d2e0cb5dd589b5
marekmical <marek.mical@gmail.com>
2025-01-26 14:01:15 +0100

    Add Client WinForms app
    Renamed server project HardkorowyKodsu to HardkorowyKodsu.Server
    Added Windows Forms application that consumes server's API
    Client app gets Tables/Views list on initialization
    Columns are acquired whenever ListBox selected index is changed
    "Refresh Table List" is available whenever the target DB schema may be altered (created/dropped a table/view)

---
+++ HardkorowyKodsu/HardkorowyKodsu.Client/Configuration/ServerConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace HardkorowyKodsu.Client.Configuration
+{
+    internal class ServerConfiguration
+    {
+        public const string APIUri = "https://localhost:7152/api/";
+        public const string TablesController = "Tables";
+        public const string ColumnsController = "Columns?name=";
+    }
+}
