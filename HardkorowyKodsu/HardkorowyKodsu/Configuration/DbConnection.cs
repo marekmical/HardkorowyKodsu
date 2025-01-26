400878e0423cf8d068f0b0f18665a5ab7f758cc9
marekmical <marek.mical@gmail.com>
2025-01-26 13:36:07 +0100

    Add Server .NET Core Web API
    Added .gitignore
    Created ASP.NET Core Web API solution
    Used Microsoft.Data.SqlClient package for communication with local SQLExpress instance
    Db connection settings are in Configuration/DbConnection.cs
    Two available controllers - TablesController and ColumnsController with single REST GET methods
    API available at https://localhost:7152
    Swagger available at https://localhost:7152/swagger/index.html

---
+++ HardkorowyKodsu/HardkorowyKodsu/Configuration/DbConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace HardkorowyKodsu.Server.Configuration
+{
+    public class DbConnection
+    {
+        public const string ConnectionString = "server=(local)\\SQLExpress;database=AdventureWorks;Trusted_Connection=True;TrustServerCertificate=True;integrated Security=SSPI;";
+    }
+}
---
+++ HardkorowyKodsu/HardkorowyKodsu/Controllers/ColumnsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace HardkorowyKodsu.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColumnsController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public ColumnsController()
+        {
+            _connectionString = Configuration.DbConnection.ConnectionString;
+        }
+
+        // GET: api/Columns
+        [HttpGet]
+        public IEnumerable<string?> GetColumns(string name)
+        {
+            using (SqlConnection _connection = new SqlConnection(_connectionString))
+            {
+                _connection.Open();
+
+                string[] restrictionValues = new string[] { null, null, name, null };
+
+                var columnList = _connection.GetSchema("Columns", restrictionValues).AsEnumerable().Select(dataRow => dataRow.Field<string>("Column_Name")).ToList();
+
+                _connection.Close();
+
+                return columnList;
+            }
+        }
+    }
+}
